0d3ddf4a10a6c7f2bc2b9fa9416d3252fe1b337b
sharvil31 <amburlesarvil@gmail.com>
2026-01-05 09:06:28 +0530

    List added items in cart 5-1-2026

--- shopping_cart/src/app/CartItems/page.js
+++ shopping_cart/src/app/CartItems/page.js
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { useSelector } from "react-redux";
 
 const CartItems = () => {
+  const itemsSelector = useSelector((state) => state.cart.items);
+
+  console.log(itemsSelector);
   return (
-    <div>page</div>
-  )
-}
+    <div className="cart-container">
+      <div className="cart-header">
+        <h1>Cart Items</h1>
+        <p>
+          <span className="font-semibold text-xl">{itemsSelector.length}</span>{" "}
+          Items
+        </p>
+      </div>
+
+      <div>
+        {itemsSelector.map((item) => (
+          <div key={item.id} className="cart-item">
+            <div className="item-info">
+              <img src={item.thumbnail} alt={item.title} />
+              <div className="item-details">
+                <p>{item.title}</p>
+                <p>{item.brand}</p>
+              </div>
+            </div>
+
+            <div className="item-actions">
+              <span className="price">{item.price}</span>
+              <button className="card-btn remove">Remove</button>
+            </div>
+          </div>
+        ))}
+
+        <div className="total-price">
+          Total: <span className="text-emerald-700">{itemsSelector.reduce((sum, curr) => sum + curr.price, 0)}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CartItems;
